1dd6ed6b64fdb0dc210ba5eb9d3f1c195fc02b76
Anam Ansari <anamansari062@gmail.com>
2024-03-13 12:58:26 +0530

    test 2

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -32,68 +32,68 @@ export default async function handler(req: any, res: any) {
       // console.log(webhook_data[0].events.nft.nfts[0])
       // let token: any = await getAsset(webhook_data[0].events.nft.nfts[0].mint)
 
-      // const response = await fetch(webhook, {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({
+      const response = await fetch(webhook, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
 
-      //     "content": null,
-      //     "embeds": [
-      //       {
-      //         "title": token.content.metadata.name + " has sold!",
-      //         "url": `https://solscan.io/token/${webhook_data[0].events.nft.nfts[0].mint}`,
-      //         "color": 16486972,
-      //         "fields": [
-      //           {
-      //             "name": "\ ",
-      //             "value": "\ "
-      //           },
-      //           {
-      //             "name": "\ ",
-      //             "value": "\ "
-      //           },
-      //           {
-      //             "name": ":moneybag:  Sale Price",
-      //             "value": "**" + (webhook_data[0].events.nft.amount / 1000000000).toFixed(2) + " " + "SOL**",
-      //             "inline": true
-      //           },
-      //           {
-      //             "name": ":date:  Sale Date",
-      //             "value": `<t:${webhook_data[0].timestamp}:R>`,
-      //             "inline": true
-      //           },
-      //           {
-      //             "name": "\ ",
-      //             "value": "\ "
-      //           },
-      //           {
-      //             "name": "Buyer",
-      //             "value": webhook_data[0].events.nft.buyer.slice(0, 4) + '..' + webhook_data[0].events.nft.buyer.slice(-4),
-      //             "inline": true
-      //           },
-      //           {
-      //             "name": "Seller",
-      //             "value": webhook_data[0].events.nft.seller.slice(0, 4) + '..' + webhook_data[0].events.nft.seller.slice(-4),
-      //             "inline": true
-      //           }
-      //         ],
-      //         "image": {
-      //           "url": token.content.files[0].uri
-      //         },
-      //         timestamp: new Date().toISOString(),
-      //         "footer": {
-      //             "text": "Helius",
-      //             "icon_url": "https://assets-global.website-files.com/641a8c4cac3aee8bd266fd58/642b5b2804ea37191a59737b_favicon-32x32.png",
-      //         }
-      //       }
-      //     ],
+          "content": null,
+          "embeds": [
+            {
+              "title": " has sold!",
+              // "url": `https://solscan.io/token/${webhook_data[0].events.nft.nfts[0].mint}`,
+              // "color": 16486972,
+              // "fields": [
+              //   {
+              //     "name": "\ ",
+              //     "value": "\ "
+              //   },
+              //   {
+              //     "name": "\ ",
+              //     "value": "\ "
+              //   },
+              //   {
+              //     "name": ":moneybag:  Sale Price",
+              //     "value": "**" + (webhook_data[0].events.nft.amount / 1000000000).toFixed(2) + " " + "SOL**",
+              //     "inline": true
+              //   },
+              //   {
+              //     "name": ":date:  Sale Date",
+              //     "value": `<t:${webhook_data[0].timestamp}:R>`,
+              //     "inline": true
+              //   },
+              //   {
+              //     "name": "\ ",
+              //     "value": "\ "
+              //   },
+              //   {
+              //     "name": "Buyer",
+              //     "value": webhook_data[0].events.nft.buyer.slice(0, 4) + '..' + webhook_data[0].events.nft.buyer.slice(-4),
+              //     "inline": true
+              //   },
+              //   {
+              //     "name": "Seller",
+              //     "value": webhook_data[0].events.nft.seller.slice(0, 4) + '..' + webhook_data[0].events.nft.seller.slice(-4),
+              //     "inline": true
+              //   }
+              // ],
+              // "image": {
+              //   "url": token.content.files[0].uri
+              // },
+              // timestamp: new Date().toISOString(),
+              // "footer": {
+              //     "text": "Helius",
+              //     "icon_url": "https://assets-global.website-files.com/641a8c4cac3aee8bd266fd58/642b5b2804ea37191a59737b_favicon-32x32.png",
+              // }
+            }
+          ],
 
-      //   },
-      //   ),
-      // });
-      // console.log(response)
+        },
+        ),
+      });
+      console.log(response)
       res.status(200).json("success")
 
     };
